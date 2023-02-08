8e28230a2357cfb458039af1baa67dd1276cef88
SAANCHI SHAH <sshah15@g.ucla.edu>
2023-02-08 14:12:28 -0800

    committing updated script

--- problemtset4.R
+++ problemtset4.R
@@ -1,1 +1,14 @@
 library(tidyverse)
+
+#Let's read in the data through read_csv without loading this data first
+mrc <- read_csv(file = url("http://www.equality-of-opportunity.org/data/college/mrc_table2.csv"))
+
+#Create a subset dataset
+mrc_subset <- mrc %>% select(name, par_q1, par_q2, par_q3, par_q4, par_q5)
+
+#Let's pivot mrc_subset now
+mrc_pivot <- mrc_subset %>% pivot_longer(cols = c(par_q1, par_q2, par_q3, par_q4, par_q5),
+                                         names_to = 'quintile',
+                                         names_prefix = 'par_',
+                                         values_to = 'fraction')
+
